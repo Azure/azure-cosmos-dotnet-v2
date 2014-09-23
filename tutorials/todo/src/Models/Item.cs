09069263d11fcbe6f7b30a8155139a1e596fa82e
Ryan CrawCour <ryancrawcour@outlook.com>
2014-09-23 15:39:32 -0700

    added serialization back to Item class

--- tutorials/todo/src/Models/Item.cs
+++ tutorials/todo/src/Models/Item.cs
@@ -7,8 +7,11 @@
     {
         [JsonProperty(PropertyName="id")]
         public string Id { get; set; }
+        [JsonProperty(PropertyName = "name")]      
         public string Name { get; set; }
+        [JsonProperty(PropertyName = "description")]        
         public string Description { get; set; }
+        [JsonProperty(PropertyName = "isComplete")]        
         public bool Completed { get; set; }
     }
 }
