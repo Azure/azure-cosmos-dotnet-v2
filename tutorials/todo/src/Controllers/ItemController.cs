8f39c17c0f502f7cb3f6237671adcc4becdfd8d1
Ryan CrawCour <ryancrawcour@outlook.com>
2014-10-13 17:15:10 -0700

    updated to include protection against CSRF attacks

--- tutorials/todo/src/Controllers/ItemController.cs
+++ tutorials/todo/src/Controllers/ItemController.cs
@@ -17,19 +17,35 @@
             return View();
         }
 
+
         [HttpPost]
-        public async Task<ActionResult> Create(Item item)
+        // To protect against Cross-Site Request Forgery, validate that the anti-forgery token was received and is valid
+        // for more details on preventing see http://go.microsoft.com/fwlink/?LinkID=517254
+        [ValidateAntiForgeryToken]
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Description,Completed")] Item item)
         {
             if (ModelState.IsValid)
             {
                 await DocumentDBRepository.CreateItemAsync(item);
                 return RedirectToAction("Index");
             }
+
             return View(item);
         }
 
+
         [HttpPost]
-        public async Task<ActionResult> Edit(Item item)
+
+        // To protect against Cross-Site Request Forgery, validate that the anti-forgery token was received and is valid
+        // for more details on preventing see http://go.microsoft.com/fwlink/?LinkID=517254
+        [ValidateAntiForgeryToken]
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.        
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Description,Completed")] Item item)
         {
             if (ModelState.IsValid)
             {
@@ -56,7 +72,30 @@
             return View(item);
         }
 
-        public async Task<ActionResult> Delete(string id)
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Item item = (Item)DocumentDBRepository.GetItem(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(item);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        // To protect against Cross-Site Request Forgery, validate that the anti-forgery token was received and is valid
+        // for more details on preventing see http://go.microsoft.com/fwlink/?LinkID=517254
+        [ValidateAntiForgeryToken]
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        public async Task<ActionResult> DeleteConfirmed([Bind(Include = "Id")] string id)
         {
             await DocumentDBRepository.DeleteItemAsync(id);
             return RedirectToAction("Index");
