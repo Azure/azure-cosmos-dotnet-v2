370fb212f895f2f241d0de8d5a98c4ecd41a8e23
arramac <arramac@microsoft.com>
2015-05-07 17:14:53 -0700

    First commit of order by samples

---
+++ samples/orderby/DocumentDB.Samples.OrderBy/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace DocumentDB.Samples.OrderBy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Documents;
+    using Microsoft.Azure.Documents.Client;
+    using Microsoft.Azure.Documents.Linq;
+    using Microsoft.Azure.Documents.Partitioning;
+    using Newtonsoft;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// This sample demonstrates basic CRUD operations on a Database resource for Azure DocumentDB.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The DocumentDB endpoint read from config. 
+        /// These values are available from the Azure Management Portal on the DocumentDB Account Blade under "Keys".
+        /// NB Keep these values in a safe and secure location. Together they provide Administrative access to your DocDB account.
+        /// </summary>
+        private static readonly string EndpointUrl = ConfigurationManager.AppSettings["EndPointUrl"];
+
+        /// <summary>
+        /// The DocumentDB authorization key.
+        /// These values are available from the Azure Management Portal on the DocumentDB Account Blade under "Keys"
+        /// </summary>
+        private static readonly string AuthorizationKey = ConfigurationManager.AppSettings["AuthorizationKey"];
+
+        /// <summary>
+        /// Database Id used for these samples.
+        /// </summary>
+        private static readonly string DatabaseId = ConfigurationManager.AppSettings["DatabaseId"];
+
+        /// <summary>
+        /// The ConnectionPolicy for these samples. Sets custom user-agent.
+        /// </summary>
+        private static readonly ConnectionPolicy ConnectionPolicy = new ConnectionPolicy { UserAgentSuffix = " samples-net-orderby/1" };
+
+        /// <summary>
+        /// The DocumentDB client instance.
+        /// </summary>
+        private DocumentClient client;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Program"/> class.
+        /// </summary>
+        /// <param name="client">The DocumentDB client instance.</param>
+        private Program(DocumentClient client)
+        {
+            this.client = client;
+        }
+
+        /// <summary>
+        /// Main method for the sample.
+        /// </summary>
+        /// <param name="args">command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            try
+            {
+                using (var client = new DocumentClient(new Uri(EndpointUrl), AuthorizationKey, ConnectionPolicy))
+                {
+                    var program = new Program(client);
+                    program.RunAsync().Wait();
+                    Console.WriteLine("Samples completed successfully.");
+                }
+            }
+#if !DEBUG
+            catch (Exception e)
+            {
+                // If the Exception is a DocumentClientException, the "StatusCode" value might help identity 
+                // the source of the problem. 
+                Console.WriteLine("Samples failed with exception:{0}", e);
+            }
+#endif
+            finally
+            {
+                Console.WriteLine("End of samples, press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// Run samples for Database create, read, update and delete.
+        /// </summary>
+        /// <returns>a Task object.</returns>
+        private async Task RunAsync()
+        {
+        }
+    }
+}
---
+++ samples/orderby/DocumentDB.Samples.OrderBy/Util/DocumentClientHelper.cs
@@ -0,0 +1,266 @@
+﻿namespace DocumentDB.Samples.OrderBy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Documents;
+    using Microsoft.Azure.Documents.Client;
+    using Microsoft.Azure.Documents.Linq;
+
+    /// <summary>
+    /// Providers common helper methods for working with DocumentClient.
+    /// </summary>
+    public class DocumentClientHelper
+    {
+        /// <summary>
+        /// Get a Database by id, or create a new one if one with the id provided doesn't exist.
+        /// </summary>
+        /// <param name="client">The DocumentDB client instance.</param>
+        /// <param name="id">The id of the Database to search for, or create.</param>
+        /// <returns>The matched, or created, Database object</returns>
+        public static async Task<Database> GetDatabaseAsync(DocumentClient client, string id)
+        {
+            Database database = client.CreateDatabaseQuery().Where(db => db.Id == id).ToArray().FirstOrDefault();
+            if (database == null)
+            {
+                database = await client.CreateDatabaseAsync(new Database { Id = id });
+            }
+
+            return database;
+        }
+
+        /// <summary>
+        /// Get a Database by id, or create a new one if one with the id provided doesn't exist.
+        /// </summary>
+        /// <param name="client">The DocumentDB client instance.</param>
+        /// <param name="id">The id of the Database to search for, or create.</param>
+        /// <returns>The matched, or created, Database object</returns>
+        public static async Task<Database> GetNewDatabaseAsync(DocumentClient client, string id)
+        {
+            Database database = client.CreateDatabaseQuery().Where(db => db.Id == id).ToArray().FirstOrDefault();
+            if (database != null)
+            {
+                await client.DeleteDatabaseAsync(database.SelfLink);
+            }
+
+            database = await client.CreateDatabaseAsync(new Database { Id = id });
+            return database;
+        }
+
+        /// <summary>
+        /// Get a DocumentCollection by id, or create a new one if one with the id provided doesn't exist.
+        /// </summary>
+        /// <param name="client">The DocumentDB client instance.</param>
+        /// <param name="database">The Database where this DocumentCollection exists / will be created</param>
+        /// <param name="collectionId">The id of the DocumentCollection to search for, or create.</param>
+        /// <returns>The matched, or created, DocumentCollection object</returns>
+        public static async Task<DocumentCollection> GetCollectionAsync(DocumentClient client, Database database, string collectionId)
+        {
+            DocumentCollection collection = client.CreateDocumentCollectionQuery(database.SelfLink)
+                .Where(c => c.Id == collectionId).ToArray().FirstOrDefault();
+
+            if (collection == null)
+            {
+                collection = await CreateDocumentCollectionWithRetriesAsync(client, database, new DocumentCollection { Id = collectionId });
+            }
+
+            return collection;
+        }
+
+        /// <summary>
+        /// Get a DocumentCollection by id, or create a new one if one with the id provided doesn't exist.
+        /// </summary>
+        /// <param name="client">The DocumentDB client instance.</param>
+        /// <param name="database">The Database where this DocumentCollection exists / will be created</param>
+        /// <param name="collectionId">The id of the DocumentCollection to search for, or create.</param>
+        /// <param name="collectionInfo">The spec/template to create collections from.</param>
+        /// <returns>The matched, or created, DocumentCollection object</returns>
+        public static async Task<DocumentCollection> GetCollectionAsync(
+            DocumentClient client,
+            Database database,
+            string collectionId,
+            DocumentCollectionInfo collectionInfo)
+        {
+            DocumentCollection collection = client.CreateDocumentCollectionQuery(database.SelfLink)
+                .Where(c => c.Id == collectionId).ToArray().FirstOrDefault();
+
+            if (collection == null)
+            {
+                collection = await CreateNewCollection(client, database, collectionId, collectionInfo);
+            }
+
+            return collection;
+        }
+
+        /// <summary>
+        /// Creates a new collection.
+        /// </summary>
+        /// <param name="client">The DocumentDB client instance.</param>
+        /// <param name="database">The Database where this DocumentCollection exists / will be created</param>
+        /// <param name="collectionId">The id of the DocumentCollection to search for, or create.</param>
+        /// <param name="collectionInfo">The spec/template to create collections from.</param>
+        /// <returns>The created DocumentCollection object</returns>
+        public static async Task<DocumentCollection> CreateNewCollection(
+            DocumentClient client,
+            Database database,
+            string collectionId,
+            DocumentCollectionInfo collectionInfo)
+        {
+            DocumentCollection collectionDefinition = new DocumentCollection { Id = collectionId };
+            if (collectionInfo != null)
+            {
+                CopyIndexingPolicy(collectionInfo, collectionDefinition);
+            }
+
+            DocumentCollection collection = await CreateDocumentCollectionWithRetriesAsync(
+                client,
+                database,
+                collectionDefinition,
+                (collectionInfo != null) ? collectionInfo.OfferType : null);
+
+            if (collectionInfo != null)
+            {
+                await RegisterScripts(client, collectionInfo, collection);
+            }
+
+            return collection;
+        }
+
+        /// <summary>
+        /// Registers the stored procedures, triggers and UDFs in the collection spec/template.
+        /// </summary>
+        /// <param name="client">The DocumentDB client.</param>
+        /// <param name="collectionInfo">The collection spec/template.</param>
+        /// <param name="collection">The collection.</param>
+        /// <returns>The Task object for asynchronous execution.</returns>
+        public static async Task RegisterScripts(DocumentClient client, DocumentCollectionInfo collectionInfo, DocumentCollection collection)
+        {
+            if (collectionInfo.StoredProcedures != null)
+            {
+                foreach (StoredProcedure sproc in collectionInfo.StoredProcedures)
+                {
+                    await client.CreateStoredProcedureAsync(collection.SelfLink, sproc);
+                }
+            }
+
+            if (collectionInfo.Triggers != null)
+            {
+                foreach (Trigger trigger in collectionInfo.Triggers)
+                {
+                    await client.CreateTriggerAsync(collection.SelfLink, trigger);
+                }
+            }
+
+            if (collectionInfo.UserDefinedFunctions != null)
+            {
+                foreach (UserDefinedFunction udf in collectionInfo.UserDefinedFunctions)
+                {
+                    await client.CreateUserDefinedFunctionAsync(collection.SelfLink, udf);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Copies the indexing policy from the collection spec.
+        /// </summary>
+        /// <param name="collectionInfo">The collection spec/template</param>
+        /// <param name="collectionDefinition">The collection definition to create.</param>
+        public static void CopyIndexingPolicy(DocumentCollectionInfo collectionInfo, DocumentCollection collectionDefinition)
+        {
+            if (collectionInfo.IndexingPolicy != null)
+            {
+                collectionDefinition.IndexingPolicy.Automatic = collectionInfo.IndexingPolicy.Automatic;
+                collectionDefinition.IndexingPolicy.IndexingMode = collectionInfo.IndexingPolicy.IndexingMode;
+
+                if (collectionInfo.IndexingPolicy.IncludedPaths != null)
+                {
+                    foreach (IndexingPath path in collectionInfo.IndexingPolicy.IncludedPaths)
+                    {
+                        collectionDefinition.IndexingPolicy.IncludedPaths.Add(path);
+                    }
+                }
+
+                if (collectionInfo.IndexingPolicy.ExcludedPaths != null)
+                {
+                    foreach (string path in collectionInfo.IndexingPolicy.ExcludedPaths)
+                    {
+                        collectionDefinition.IndexingPolicy.ExcludedPaths.Add(path);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Create a DocumentCollection, and retry when throttled.
+        /// </summary>
+        /// <param name="client">The DocumentDB client instance.</param>
+        /// <param name="database">The database to use.</param>
+        /// <param name="collectionDefinition">The collection definition to use.</param>
+        /// <param name="offerType">The offer type for the collection.</param>
+        /// <returns>The created DocumentCollection.</returns>
+        public static async Task<DocumentCollection> CreateDocumentCollectionWithRetriesAsync(
+            DocumentClient client,
+            Database database,
+            DocumentCollection collectionDefinition,
+            string offerType = "S1")
+        {
+            return await ExecuteWithRetries(
+                client,
+                () => client.CreateDocumentCollectionAsync(
+                        database.SelfLink,
+                        collectionDefinition,
+                        new RequestOptions
+                        {
+                            OfferType = offerType
+                        }));
+        }
+
+        /// <summary>
+        /// Execute the function with retries on throttle.
+        /// </summary>
+        /// <typeparam name="V">The type of return value from the execution.</typeparam>
+        /// <param name="client">The DocumentDB client instance.</param>
+        /// <param name="function">The function to execute.</param>
+        /// <returns>The response from the execution.</returns>
+        public static async Task<V> ExecuteWithRetries<V>(DocumentClient client, Func<Task<V>> function)
+        {
+            TimeSpan sleepTime = TimeSpan.Zero;
+
+            while (true)
+            {
+                try
+                {
+                    return await function();
+                }
+                catch (DocumentClientException de)
+                {
+                    if ((int)de.StatusCode != 429)
+                    {
+                        throw;
+                    }
+
+                    sleepTime = de.RetryAfter;
+                }
+                catch (AggregateException ae)
+                {
+                    if (!(ae.InnerException is DocumentClientException))
+                    {
+                        throw;
+                    }
+
+                    DocumentClientException de = (DocumentClientException)ae.InnerException;
+                    if ((int)de.StatusCode != 429)
+                    {
+                        throw;
+                    }
+
+                    sleepTime = de.RetryAfter;
+                }
+
+                await Task.Delay(sleepTime);
+            }
+        }
+    }
+}
