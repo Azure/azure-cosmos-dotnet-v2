cee12c606226fe878019c9240a6f1976670ef6a7
AndrewHoh <Andy.Hoh@gmail.com>
2015-09-14 12:15:15 -0700

    removing extra dependencies

--- tutorials/get-started/src/Program.cs
+++ tutorials/get-started/src/Program.cs
@@ -12,10 +12,8 @@
 namespace DocumentDB.GetStarted
 {
     using System;
-    using System.Collections.Generic;
     using System.Configuration;
     using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
 
     // Add DocumentDB references
