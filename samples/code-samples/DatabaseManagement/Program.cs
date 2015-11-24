0314fd60e1a1fef4b4afb6a17f56a2cc701c862e
ryancrawcour <ryancraw@microsoft.com>
2015-11-23 16:37:18 -0800

    simplified & updated database samples

--- samples/code-samples/DatabaseManagement/Program.cs
+++ samples/code-samples/DatabaseManagement/Program.cs
@@ -4,7 +4,6 @@
     using Microsoft.Azure.Documents.Client;
     using Microsoft.Azure.Documents.Linq;
     using System;
-    using System.Collections.Generic;
     using System.Configuration;
     using System.Linq;
     using System.Threading.Tasks;
@@ -31,7 +30,7 @@
                 //Setup a single instance of DocumentClient that is reused throughout the application
                 using (client = new DocumentClient(new Uri(endpointUrl), authorizationKey))
                 {
-                    RunAsync().Wait();
+                    DatabaseManagementAsync().Wait();
                 }
             }
             catch (DocumentClientException de)
@@ -51,65 +50,39 @@
             }
         }
 
-        private static async Task RunAsync()
+        private static async Task DatabaseManagementAsync()
         {
             //Try to get a database
+            //Note: we are using query here instead of ReadDatabaseAsync because we're checking if something exists
+            //      the ReadDatabaseAsync method expects the resource to be there, if its not we will get an error
+            //      instead of an empty 
             Database database = client.CreateDatabaseQuery().Where(db => db.Id == databaseId).AsEnumerable().FirstOrDefault();
             
-            //Create database
-            //First check if a database was returned, if not then create it
+            //First check if a database was returned
             if (database==null)
             {
+                //Create database
                 database = await client.CreateDatabaseAsync(new Database { Id = databaseId });
                 Console.WriteLine("Created Database: id - {0} and selfLink - {1}", database.Id, database.SelfLink);
             }
             
-            //List databases for an account
-            var databases = await ListDatabasesAsync();
+            //Get a single database
+            //Note: that we don't need to use the SelfLink of a Database anymore
+            //      the links for a resource are now comprised of their ID properties
+            //      using UriFactory will give you the correct URI for a resource
+            //
+            //      SelfLink will still work if you're already using this
+            database = await client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(databaseId));
+            
+            //List all databases for an account
+            var databases = await client.ReadDatabaseFeedAsync();
             foreach (var db in databases)
             {
                 Console.WriteLine(db);    
             }
             
             //Delete a database
-            //Cleanup using the SelfLink property of the Database which we either retrieved or created
-            //If you do not have this SelfLink property accessible and populated you would need to get the Database using the id, 
-            //then read the SelfLink property from that. This SelfLink value never changes for a Database once created;
-            //so it would be perfectly acceptable practice to cache the value or store this in your configuratiom files
-            await client.DeleteDatabaseAsync(database.SelfLink);
-        }
-
-        /// <summary>
-        /// This method uses a ReadDatabaseFeedAsync method to read a list of all databases on the account.
-        /// It demonstrates a pattern for how to control paging and deal with continuations
-        /// This should not be needed for reading a list of databases as there are unlikely to be many hundred,
-        /// but this same pattern is introduced here and can be used on other ReadFeed methods.
-        /// </summary>
-        /// <returns>A List of Database entities</returns>
-        private static async Task<List<Database>> ListDatabasesAsync()
-        {
-            string continuation = null;
-            List<Database> databases = new List<Database>();
-
-            do
-            {
-                FeedOptions options = new FeedOptions
-                {
-                    RequestContinuation = continuation,
-                    MaxItemCount = 50
-                };
-
-                FeedResponse<Database> response = await client.ReadDatabaseFeedAsync(options);
-                foreach (Database db in response)
-                {
-                    databases.Add(db);
-                }
-
-                continuation = response.ResponseContinuation;
-            } 
-            while (!String.IsNullOrEmpty(continuation));
-
-            return databases;
+            await client.DeleteDatabaseAsync(UriFactory.CreateDatabaseUri(databaseId));
         }        
     }
 }
