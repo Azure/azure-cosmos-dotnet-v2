125976b573d3d87f9e5f3f518cc0161dc4d15045
REDMOND\rafats <rafats@microsoft.com>
2017-10-10 10:10:55 -0700

    A very simplified example of Processor library use

---
+++ samples/code-samples/ChangeFeedProcessor/DocumentFeedObserverFactory.cs
@@ -0,0 +1,49 @@
+﻿//--------------------------------------------------------------------------------- 
+// <copyright file="DocumentFeedObserverFactory.cs" company="Microsoft">
+// Microsoft (R)  Azure SDK 
+// Software Development Kit 
+//  
+// Copyright (c) Microsoft Corporation. All rights reserved.   
+// 
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,  
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES  
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  
+// </copyright>
+//---------------------------------------------------------------------------------
+
+namespace ChangeFeedMigrationSample
+{
+    using Microsoft.Azure.Documents.ChangeFeedProcessor;
+    using Microsoft.Azure.Documents.Client;
+
+    /// <summary>
+    /// Factory class to create instance of document feed observer. 
+    /// </summary>
+    public class DocumentFeedObserverFactory : IChangeFeedObserverFactory
+    {
+        private DocumentClient client;
+        private DocumentCollectionInfo collectionInfo;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DocumentFeedObserverFactory" /> class.
+        /// Saves input DocumentClient and DocumentCollectionInfo parameters to class fields
+        /// </summary>
+        /// <param name="destClient">Client connected to destination collection</param>
+        /// <param name="destCollInfo">Destination collection information</param>
+        public DocumentFeedObserverFactory(DocumentClient destClient, DocumentCollectionInfo destCollInfo)
+        {
+            this.collectionInfo = destCollInfo;
+            this.client = destClient;
+        }
+
+        /// <summary>
+        /// Creates document observer instance with client and destination collection information
+        /// </summary>
+        /// <returns>DocumentFeedObserver with client and destination collection information</returns>
+        public IChangeFeedObserver CreateObserver()
+        {
+            DocumentFeedObserver newObserver = new DocumentFeedObserver(this.client, this.collectionInfo);
+            return newObserver;
+        }
+    }
+}
