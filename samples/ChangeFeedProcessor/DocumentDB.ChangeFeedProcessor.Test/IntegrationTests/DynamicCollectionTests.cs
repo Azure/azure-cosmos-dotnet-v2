1a91d07c906528fb8f550f2e6fd299f62f1c86a3
mkolt <mkolt@microsoft.com>
2018-01-22 10:49:18 -0800

    Test for ChangeFeedOptions.StartTime. (#420)

--- samples/ChangeFeedProcessor/DocumentDB.ChangeFeedProcessor.Test/IntegrationTests/DynamicCollectionTests.cs
+++ samples/ChangeFeedProcessor/DocumentDB.ChangeFeedProcessor.Test/IntegrationTests/DynamicCollectionTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -58,7 +59,8 @@ namespace DocumentDB.ChangeFeedProcessor.Test.IntegrationTests
                 new ChangeFeedHostOptions());
             await host.RegisterObserverFactoryAsync(observerFactory);
 
-            var isStartOk = allObserversStarted.WaitOne(Debugger.IsAttached ? changeWaitTimeout + changeWaitTimeout : changeWaitTimeout);
+            var isStartOk = allObserversStarted.WaitOne(
+                Debugger.IsAttached ? IntegrationTest.changeWaitTimeout + IntegrationTest.changeWaitTimeout : IntegrationTest.changeWaitTimeout);
             Assert.IsTrue(isStartOk, "Timed out waiting for observres to start");
 
             using (var client = new DocumentClient(this.ClassData.monitoredCollectionInfo.Uri, this.ClassData.monitoredCollectionInfo.MasterKey, this.ClassData.monitoredCollectionInfo.ConnectionPolicy))
@@ -69,7 +71,7 @@ namespace DocumentDB.ChangeFeedProcessor.Test.IntegrationTests
                     documentCount);
             }
 
-            allDocsProcessed.WaitOne(changeWaitTimeout);
+            allDocsProcessed.WaitOne(IntegrationTest.changeWaitTimeout);
 
             try
             {
@@ -78,6 +80,63 @@ namespace DocumentDB.ChangeFeedProcessor.Test.IntegrationTests
             finally
             {
                 await host.UnregisterObserversAsync();
+            }
+        }
+
+        [TestMethod]
+        public async Task TestStartTime()
+        {
+            var collectionUri = UriFactory.CreateDocumentCollectionUri(this.ClassData.monitoredCollectionInfo.DatabaseName, this.ClassData.monitoredCollectionInfo.CollectionName);
+            using (var client = new DocumentClient(this.ClassData.monitoredCollectionInfo.Uri, this.ClassData.monitoredCollectionInfo.MasterKey, this.ClassData.monitoredCollectionInfo.ConnectionPolicy))
+            {
+                await client.CreateDocumentAsync(collectionUri, JsonConvert.DeserializeObject("{\"id\": \"doc1\"}"));
+
+                // In worst case (long transaction, heavy load, the atomicity of StartTime is 5 sec).
+                // For this case (different transactions) it's OK to wait timestamp precision time.
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                DateTime timeInBeweeen = DateTime.Now;
+                await Task.Delay(TimeSpan.FromSeconds(1));
+
+                await client.CreateDocumentAsync(collectionUri, JsonConvert.DeserializeObject("{\"id\": \"doc2\"}"));
+
+                int partitionCount = await Helper.GetPartitionCount(this.ClassData.monitoredCollectionInfo);
+                var allDocsProcessed = new ManualResetEvent(false);
+
+                var processedDocs = new List<Document>();
+                var observerFactory = new TestObserverFactory(
+                    null,
+                    null,
+                    (context, docs) => 
+                    {
+                        processedDocs.AddRange(docs);
+                        foreach (var doc in docs)
+                        {
+                            if (doc.Id == "doc2") allDocsProcessed.Set();
+                        }
+                        return Task.CompletedTask;
+                    });
+
+                var host = new ChangeFeedEventHost(
+                    Guid.NewGuid().ToString(),
+                    this.ClassData.monitoredCollectionInfo,
+                    this.LeaseCollectionInfo,
+                    new ChangeFeedOptions { StartTime = timeInBeweeen },
+                    new ChangeFeedHostOptions());
+                await host.RegisterObserverFactoryAsync(observerFactory);
+
+                var isStartOk = allDocsProcessed.WaitOne(
+                    Debugger.IsAttached ? IntegrationTest.changeWaitTimeout + IntegrationTest.changeWaitTimeout : IntegrationTest.changeWaitTimeout);
+                Assert.IsTrue(isStartOk, "Timed out waiting for docs to process");
+
+                try
+                {
+                    Assert.AreEqual(1, processedDocs.Count, "Wrong processed count");
+                    Assert.AreEqual("doc2", processedDocs[0].Id, "Wrong doc.id");
+                }
+                finally
+                {
+                    await host.UnregisterObserversAsync();
+                }
             }
         }
     }
