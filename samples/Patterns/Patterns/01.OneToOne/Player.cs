e47699faa8bcd6c46de8e2325eee2ca97bf7a96f
Aravind Krishna <arramac@microsoft.com>
2018-03-06 08:27:53 -0800

    add samples for patterns

---
+++ samples/Patterns/Patterns/01.OneToOne/Player.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.OneToOne
+{
+    class Player
+    {
+        [JsonProperty("id")]
+        public String Id { get; set; }
+
+        [JsonProperty("name")]
+        public String Name { get; set; }
+
+        [JsonProperty("handle")]
+        public String Handle { get; set; }
+
+        [JsonProperty("highScore")]
+        public Double HighScore { get; set; }
+
+        [JsonProperty("lastLogin")]
+        public DateTime LastLogin { get; set; }
+
+        [JsonProperty("_etag")]
+        public String ETag { get; set; }
+    }
+}
