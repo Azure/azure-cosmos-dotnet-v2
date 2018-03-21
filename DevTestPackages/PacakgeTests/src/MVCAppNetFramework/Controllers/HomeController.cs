2440c9cfda4b628908cddae69267eadcc75af0d9
REDMOND\gaausfel <gaausfel@microsoft.com>
2018-03-21 16:48:07 -0700

    Add MVC App Test and change package version to preview

---
+++ DevTestPackages/PacakgeTests/src/MVCAppNetFramework/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using SmokeTestLib.NetStandard;
+
+namespace MVCAppNetFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var binDir = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "bin");
+
+            var nativeDll = new List<string>() {
+                "Microsoft.Azure.Documents.ServiceInterop.dll",
+                "DocumentDB.Spatial.Sql.dll",
+            };
+
+            foreach (var dll in nativeDll)
+            {                
+                var dllPath = Path.Combine(binDir, dll);
+                if (!new FileInfo(dllPath).Exists)
+                {                    
+                    throw new Exception($"Missing - {dllPath}");
+                }                
+            }
+
+            var test = new SmokeTest();
+            await test.RunDemoAsync();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DevTestPackages/PacakgeTests/src/WebAppNetCore/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SmokeTestLib.NetStandard;
+using WebAppNetCore.Models;
+
+namespace WebAppNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var binDir = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory);
+
+            var nativeDll = new List<string>() {
+                "Microsoft.Azure.Documents.ServiceInterop.dll",
+                "DocumentDB.Spatial.Sql.dll",
+            };
+
+            foreach (var dll in nativeDll)
+            {
+                var dllPath = Path.Combine(binDir, dll);
+                if (!new FileInfo(dllPath).Exists)
+                {
+                    throw new Exception($"Missing - {dllPath}");
+                }
+            }
+
+            var test = new SmokeTest();
+            await test.RunDemoAsync();
+            return View();            
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
