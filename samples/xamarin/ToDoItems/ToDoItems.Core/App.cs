1b61bc6038ef316e17dba94f1e9508a240ae2aac
Matt Soucoup <msoucoup@codemilltech.com>
2018-03-27 09:03:55 -0500

    Large titles

--- samples/xamarin/ToDoItems/ToDoItems.Core/App.cs
+++ samples/xamarin/ToDoItems/ToDoItems.Core/App.cs
@@ -1,21 +1,37 @@
 ﻿using System;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
 namespace ToDoItems.Core
 {
     public class App : Application
     {
         public App()
         {
+            var toDoNavPage = new Xamarin.Forms.NavigationPage(new ToDoItemsPage())
+            {
+                Title = "To Do",
+                BarBackgroundColor = Color.FromHex("#2082fa"),
+                BarTextColor = Color.White
+            };
+            toDoNavPage.On<Xamarin.Forms.PlatformConfiguration.iOS>().SetPrefersLargeTitles(true);
+
+            var completedNavPage = new Xamarin.Forms.NavigationPage(new CompletedItemsPage())
+            {
+                Title = "Complete",
+                BarBackgroundColor = Color.FromHex("#2082fa"),
+                BarTextColor = Color.White
+            };
+            completedNavPage.On<Xamarin.Forms.PlatformConfiguration.iOS>().SetPrefersLargeTitles(true);
+
             var mainTabbedPage = new TabbedPage
             {
                 Children = {
-                    new NavigationPage(new ToDoItemsPage()) { Title="To Do"},
-                    new NavigationPage(new CompletedItemsPage()){ Title="Complete"}
+                    toDoNavPage, completedNavPage
                 }
             };
 
             MainPage = mainTabbedPage;
-            //MainPage = new NavigationPage(new ToDoItemsPage());
         }
     }
 }
--- samples/xamarin/ToDoItems/iOS/AppDelegate.cs
+++ samples/xamarin/ToDoItems/iOS/AppDelegate.cs
@@ -13,7 +13,6 @@ namespace ToDoItems.iOS
     {
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-
             global::Xamarin.Forms.Forms.Init();
 
             LoadApplication(new App());
