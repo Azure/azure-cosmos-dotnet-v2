5d080d40cfc7816fb34f2a42905a270db5f89238
REDMOND\rafats <rafats@microsoft.com>
2018-08-06 18:26:06 -0700

    RemovingChangeFeedoptions

--- samples/code-samples/ChangeFeedProcessorV2/Program.cs
+++ samples/code-samples/ChangeFeedProcessorV2/Program.cs
@@ -117,19 +117,12 @@ namespace ChangeFeedProcessor
                 CollectionName = this.leaseCollectionName
             };
             DocumentFeedObserverFactory docObserverFactory = new DocumentFeedObserverFactory();
-            ChangeFeedOptions feedOptions = new ChangeFeedOptions();
-
-            /* ie customize StartFromBeginning so change feed reads from beginning
-                can customize MaxItemCount, PartitonKeyRangeId, RequestContinuation, SessionToken and StartFromBeginning
-            */
-
-            feedOptions.StartFromBeginning = true;
-
             ChangeFeedProcessorOptions feedProcessorOptions = new ChangeFeedProcessorOptions();
 
             // ie. customizing lease renewal interval to 15 seconds
             // can customize LeaseRenewInterval, LeaseAcquireInterval, LeaseExpirationInterval, FeedPollDelay 
             feedProcessorOptions.LeaseRenewInterval = TimeSpan.FromSeconds(15);
+            feedProcessorOptions.StartFromBeginning = true;
             ChangeFeedProcessorBuilder builder = new ChangeFeedProcessorBuilder();
             builder
                 .WithHostName(hostName)
